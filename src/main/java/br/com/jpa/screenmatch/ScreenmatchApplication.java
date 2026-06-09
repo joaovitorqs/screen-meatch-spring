c123fa4289a735e17f4f5d31cb8788051ec883e5
João Vitor <joao_vqs@hotmail.com>
2026-06-09 12:55:54 -0300

    Add search in main

--- src/main/java/br/com/jpa/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/jpa/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.jpa.screenmatch;
 
+import br.com.jpa.screenmatch.model.DadosSerie;
+import br.com.jpa.screenmatch.service.ConsumoApi;
+import br.com.jpa.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=e5b2055");
+        System.out.println(json);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
     }
 }
