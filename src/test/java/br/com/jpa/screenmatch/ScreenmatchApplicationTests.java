1da8ef11986b1a0a760eb0e44d279c8dfc42e923
João Vitor <joao_vqs@hotmail.com>
2026-06-09 10:29:44 -0300

    First commit

---
+++ src/test/java/br/com/jpa/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jpa.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
